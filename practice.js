6e24b84027d77ee5a0a595183abe39591b77e06c
Archive 15 Design <126675102+Archive15Design@users.noreply.github.com>
2023-05-09 16:42:26 +0200

    problems solved

--- practice.js
+++ practice.js
@@ -1,45 +1,35 @@
+// each function creates sorted variables by calling findLargest().
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  let [smallNum, bigNum] = findLargest(num1, num2);
+  return bigNum * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  let [smallNum, bigNum] = findLargest(num1, num2);
+  return bigNum / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  let [smallNum, bigNum] = findLargest(sum1, sum2);
+  return `I ate ${bigNum} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+  let [smallDog, bigDog] = findLargest(weight1, weight2);
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
+}
+
+// findLargest returns a sorted array with the smallest number first.
+function findLargest(num1, num2){
+  let sortedNums = []
+  if (num1 > num2){
+     sortedNums.push(num2);
+     sortedNums.push(num1);
   } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+    sortedNums.push(num1);
+    sortedNums.push(num2);
   }
+  return sortedNums;
 }
 
 
